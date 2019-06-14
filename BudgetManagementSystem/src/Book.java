f7f735e9848433defc570e41aa1433c4596bc7d4
finnluii <22249550+finnluii@users.noreply.github.com>
2019-06-14 00:57:02 -0400

    Cleaned up main with getters and setters

--- BudgetManagementSystem/src/Book.java
+++ BudgetManagementSystem/src/Book.java
@@ -1,4 +1,6 @@
 import java.sql.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Book {
 
@@ -8,27 +10,65 @@ public class Book {
     private double cost;
 
     // Constructor method
-    public Book(String bookTitle, String bookAuthor, double bookPrice) {
-        title = bookTitle;
-        author = bookAuthor;
-//        if (bookPrice < 0) {
-//            throw new IllegalArgumentException("Invalid price. Must be greater than $0.");
-//        } else {
-//            cost = bookPrice;
-//        }
-        cost = bookPrice;
+    public Book(String bookTitle) {
+        this.title = bookTitle;
+        this.author = setAuthor();
+        this.cost = setCost();
+        System.out.println("The new book you entered is named " + this.title + " and it is written by " + this.author
+        + " and it costed " + this.cost);
     }
 
     public String getTitle(){
         return this.title;
     }
 
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public String getAuthor() {
+        return this.author;
+    }
+
+    private String setAuthor() {
+        System.out.println("Enter the name of the author.");
+        Scanner in = new Scanner(System.in);
+        String newAuthor = in.nextLine().trim();
+        while (newAuthor.isEmpty()) {
+            System.out.println("You must not leave this field empty!");
+            newAuthor = in.nextLine().trim();
+        }
+        return newAuthor;
+    }
+
     public double getCost() {
         return this.cost;
     }
 
-    public void setCost(double newCost) {
-        this.cost = newCost;
+    public double setCost() {
+        Scanner in = new Scanner(System.in);
+        double newCost = 0;
+        while (true) {
+            try {
+                System.out.println("How much did it cost?");
+                in = new Scanner(System.in);
+                newCost = in.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a number!");
+            }
+        }
+        while (newCost < 0) {
+            try {
+                System.out.println("The cost of the book cannot be negative. Please try again!");
+                in = new Scanner(System.in);
+                newCost = in.nextDouble();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a positive number");
+            }
+        }
+        return newCost;
     }
 
 }
--- BudgetManagementSystem/src/Main.java
+++ BudgetManagementSystem/src/Main.java
@@ -1,55 +1,134 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.sql.*;
 
 public class Main {
-
     public static void main (String args[ ]) {
-//        Book book = new Book("Harry Potter", "JK Rowling", 29.99);
 
         Scanner scanner = new Scanner(System.in);
 
-        /* Title */
-        System.out.println("Tell me about the book you bought today. What's the name?");
-//        String title = scanner.nextLine().replaceAll("\\s+","");
-        String title = scanner.nextLine().trim();
-        while (title.isEmpty()) {
-            System.out.println("You must enter a title!");
-            title = scanner.nextLine().replaceAll("\\s+","");
+        boolean exit = true;
+        int input_num;
+        System.out.println("Input 'add' if you would like a new book that you read.");
+        System.out.println("Input 'update' if you would like to update an existing book you've read.");
+        System.out.println("Input 'display' to see all books saved.");
+        System.out.println("Input 'delete' to delete a book.");
+        System.out.println("Input 'exit' if you would like to exit the application.");
+
+        while (exit){
+
+            String input = scanner.nextLine().trim();
+            exit = false;
+
+            switch (input) {
+                case "add":
+                    /* Title */
+                    System.out.println("Great. What book do you want to add to your collection?");
+                    String title = scanner.nextLine().trim();
+                    while (title.isEmpty()) {
+                        System.out.println("You must enter a title!");
+                        title = scanner.nextLine().trim();
+                    }
+
+//                    /* Author */
+//                    System.out.println("Who's the author(s)?");
+//                    String author = scanner.nextLine().trim();
+//                    while (author.isEmpty()) {
+//                        System.out.println("You must enter at least one author!");
+//                        author = scanner.nextLine().trim();
+//                    }
+//
+//                    /* Cost */
+//                    double cost = 0;
+//                    while (true) {
+//                        try {
+//                            System.out.println("How much did it cost?");
+//                            scanner = new Scanner(System.in);
+//                            cost = scanner.nextDouble();
+//                            break;
+//                        } catch (InputMismatchException e) {
+//                            System.out.println("Please enter a number!");
+//                        }
+//                    }
+//
+//                    System.out.println(cost);
+//
+//                    System.out.println(title);
+//                    System.out.println(author);
+//                    Book book = new Book(title, author, cost);
+                    Book book = new Book(title);
+
+                    input_num = 0;
+                    break;
+                case "update":
+                    input_num = 1;
+                    break;
+                case "display":
+                    input_num = 2;
+                    break;
+                case "delete":
+                    input_num = 3;
+                    break;
+                case "exit":
+                    input_num = 4;
+                    break;
+                default:
+                    exit = true;
+                    System.out.println("Invalid option. Please try again!");
+                    break;
+
+            }
         }
 
-        /* Author */
-        System.out.println("Who's the author(s)?");
-        String author = scanner.nextLine().replaceAll("\\s+","");
-        while (author.isEmpty()) {
-            System.out.println("You must enter at least one author!");
-            author = scanner.nextLine().replaceAll("\\s+","");
-        }
+        System.exit(-1);
 
-        /* Cost */
-        System.out.println("How much did it cost?");
-        double cost = scanner.nextDouble();
-        System.out.print(cost);
 
-        System.out.println(title);
-        System.out.println(author);
-        Book book = new Book(title, author, cost);
 
-        // Database connection
-        /*
-        Connection c = null;
 
-        try {
-            Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:test.db");
-        } catch ( Exception e ) {
-            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }
-        System.out.println("Opened database successfully");
-        */
 
+
+//        // Database connection
+//        Connection c = null;
+//
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//            c = DriverManager.getConnection("jdbc:sqlite:books.db");
+//
+//
+//        } catch ( Exception e ) {
+//            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+//            System.exit(1);
+//        }
+//
+//        // Successfully opened database
+//        try {
+//            Statement stmt = c.createStatement();
+//            String sql_create_table = "CREATE TABLE BOOKS(" +
+//                    "ISBN INT PRIMARY KEY NOT NULL, " +
+//                    "TITLE TEXT NOT NULL, " +
+//                    "AUTHOR TEXT" +
+//                    ")";
+//            stmt.executeUpdate((sql_create_table));
+//            stmt.close();
+//            c.close();
+//
+//        } catch (SQLException e) {
+//            System.out.println("Table BOOKS already exists.");
+//        }
+//
+//        try {
+//            Statement stmt = c.createStatement();
+//            String sql_insert_book = "SELECT COUNT(*) FROM BOOKS WHERE TITLE='harry' AND AUTHOR='rowling'";
+//
+//            stmt.executeUpdate(sql_insert_book);
+//
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//            System.exit(-1);
+//        }
 
     }
 }
